70ed59f8fd3f4cead8e0d95e63ddcc82d2bff7ab
Daniel Hernández <danielht97-@outlook.com>
2019-02-18 08:37:06 -0600

    Se creo la prueba para medicos

--- hospitales/medicos/tests.py
+++ hospitales/medicos/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestCatalogo(TestCase):
+
+    def test_smoke(self):
+        self.assertEquals(2, 1+1)
+
+    def test_catalgo(self):
+        self.assertFalse(False)
