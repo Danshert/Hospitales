fec938f3f4374b14ad4ec112483215b333f76483
Desarrollador de videojuegos <apinedobarrios@live.com>
2019-02-20 08:46:43 -0600

    Corregí las tabulaciones y los errores detectados por flake

--- hospitales/pacientes/tests.py
+++ hospitales/pacientes/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
 class TestPacientes(TestCase):
 
-	def test_smoke(self):
-		self.assertEquals(2,2)
+    def test_smoke(self):
+        self.assertEquals(2, 2)
